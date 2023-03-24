469f7562008cd361ca41b79fbc5cf0756728495e
Daniel Gabriel <reelpscanal@gmail.com>
2023-03-23 23:43:25 -0300

    fix eslint

--- app/(dashboard)/dashboard/settings/page.tsx
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -1,21 +1,21 @@
-import { redirect } from "next/navigation"
+import { redirect } from 'next/navigation'
 
-import { authOptions } from "@/lib/auth"
-import { getCurrentUser } from "@/lib/session"
-import { DashboardHeader } from "@/components/header"
-import { DashboardShell } from "@/components/shell"
-import { UserNameForm } from "@/components/user-name-form"
+import { authOptions } from '@/lib/auth'
+import { getCurrentUser } from '@/lib/session'
+import { DashboardHeader } from '@/components/header'
+import { DashboardShell } from '@/components/shell'
+import { UserNameForm } from '@/components/user-name-form'
 
 export const metadata = {
-  title: "Settings",
-  description: "Manage account and website settings.",
+  title: 'Settings',
+  description: 'Manage account and website settings.',
 }
 
 export default async function SettingsPage() {
   const user = await getCurrentUser()
 
   if (!user) {
-    redirect(authOptions?.pages?.signIn || "/login")
+    redirect(authOptions?.pages?.signIn || '/login')
   }
 
   return (
